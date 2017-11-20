f76348d291971c5e184fa461fd88f714e4299bf2
melchior <melchior_note@163.com>
2017-11-20 23:02:29 +0800

    smooth

---
+++ test/008_smooth_images/build/CMakeFiles/smooth_img.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/smooth_img.dir/Smoothing.o"
+  "smooth_img.pdb"
+  "smooth_img"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/smooth_img.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
