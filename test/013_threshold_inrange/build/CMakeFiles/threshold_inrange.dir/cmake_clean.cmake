60fc02bc2cece6571d12c25147bfcc9d3321956d
melchior <melchior_note@163.com>
2017-11-22 21:53:24 +0800

    threshold inrange

---
+++ test/013_threshold_inrange/build/CMakeFiles/threshold_inrange.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/threshold_inrange.dir/Threshold_inRange.o"
+  "threshold_inrange.pdb"
+  "threshold_inrange"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/threshold_inrange.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
