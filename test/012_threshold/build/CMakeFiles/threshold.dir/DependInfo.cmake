f3cdd188a28e2ff63b41d0ed0707596d041ca465
melchior <melchior_note@163.com>
2017-11-22 21:19:31 +0800

    threshold

---
+++ test/012_threshold/build/CMakeFiles/threshold.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/melchior/opencv3.3.1_imgproc/test/012_threshold/Threshold.cpp" "/home/melchior/opencv3.3.1_imgproc/test/012_threshold/build/CMakeFiles/threshold.dir/Threshold.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/melchior/opencv-3.3.0/build"
+  "/home/melchior/opencv-3.3.0/include"
+  "/home/melchior/opencv-3.3.0/include/opencv"
+  "/home/melchior/opencv-3.3.0/modules/core/include"
+  "/home/melchior/opencv-3.3.0/modules/flann/include"
+  "/home/melchior/opencv-3.3.0/modules/imgproc/include"
+  "/home/melchior/opencv-3.3.0/modules/ml/include"
+  "/home/melchior/opencv-3.3.0/modules/objdetect/include"
+  "/home/melchior/opencv-3.3.0/modules/photo/include"
+  "/home/melchior/opencv-3.3.0/modules/video/include"
+  "/home/melchior/opencv-3.3.0/modules/dnn/include"
+  "/home/melchior/opencv-3.3.0/modules/imgcodecs/include"
+  "/home/melchior/opencv-3.3.0/modules/shape/include"
+  "/home/melchior/opencv-3.3.0/modules/videoio/include"
+  "/home/melchior/opencv-3.3.0/modules/highgui/include"
+  "/home/melchior/opencv-3.3.0/modules/superres/include"
+  "/home/melchior/opencv-3.3.0/modules/ts/include"
+  "/home/melchior/opencv-3.3.0/modules/features2d/include"
+  "/home/melchior/opencv-3.3.0/modules/calib3d/include"
+  "/home/melchior/opencv-3.3.0/modules/stitching/include"
+  "/home/melchior/opencv-3.3.0/modules/videostab/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
