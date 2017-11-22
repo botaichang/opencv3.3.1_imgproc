ad73bdc3fb80f90ed90893d7bee4508cddd3e2e7
melchior <melchior_note@163.com>
2017-11-22 22:48:44 +0800

    sobel

---
+++ test/016_sobel/build/CMakeFiles/project_name.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/project_name.dir/Sobel_Demo.o"
+  "project_name.pdb"
+  "project_name"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/project_name.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
